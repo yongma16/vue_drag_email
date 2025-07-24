edc781926a1ea694623e31f8f7d0e8f4c98296ef
mayong <v_bymyma@tencent.com>
2025-07-24 17:55:56 +0800

    feat: 初始化

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
+const state=reactive({
+  title:'Vue Drag Email',
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+// @ts-ignore
+import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  // 打包相对路径
+  base: './',
+  server: {
+    port: 7777,
+    open: true,
+    cors: true
+    // proxy: {
+    //   ...
+    // }
+  },
+  resolve: {
+    alias: {
+      // @ts-ignore
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  plugins: [vue()]
+});
